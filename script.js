044b01be685c8549a93a7f9020d478bc00eabc02
Luka Popovic <lukapopovic0111@gmail.com>
2025-08-23 20:41:00 +0200

    implement getWeather() with OpenWeather API

--- script.js
+++ script.js
@@ -25,3 +25,23 @@ let month = months[dateObj.getUTCMonth()];
 let day = dateObj.getUTCDate() - 1;
 let year = dateObj.getUTCFullYear();
 date.innerHTML = `${month} ${day}, ${year}`;
+const app = document.getElementById("app");
+const getWeather = async () => {
+  try {
+    const cityName = document.getElementById("searchBarInput").value;
+    const weatherDataFetch = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${
+        cityName ? cityName : "Belgrade"
+      }&appid=d4528262aeac1db4e8f1f3d7b2335a65&units=metric`,
+      {
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
+    const weatherData = await weatherDataFetch.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+getWeather();
