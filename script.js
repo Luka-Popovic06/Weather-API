9bb317310e460f5b1a7993ccb7ab7c9fbaef7c46
Luka Popovic <lukapopovic0111@gmail.com>
2025-08-23 20:38:31 +0200

    add date rendering to header

---
+++ script.js
@@ -0,0 +1,22 @@
+const date = document.getElementById("date");
+const city = document.getElementById("city");
+const temp = document.getElementById("temp");
+const tempImg = document.getElementById("tempImg");
+const description = document.getElementById("description");
+const tempMax = document.getElementById("tempMax");
+const tempMin = document.getElementById("tempMin");
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "'June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
